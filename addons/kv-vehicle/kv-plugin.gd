2217e3bf491104d6268b290097cf4ce7cd186256
kurtzmusch <kurtzmusch@gmail.com>
2023-12-12 11:24:49 -0300

    filename refactor

--- addons/kv-vehicle/kv-plugin.gd
+++ addons/kv-vehicle/kv-plugin.gd
@@ -3,10 +3,10 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type('KVVehicle', 'KVVehicle', preload("res://addons/kv-vehicle/scene/instantiable/kvehicle.gd"), preload("res://addons/kv-vehicle/city-car.svg"))
-	add_custom_type('KVWheel', 'KVWheel', preload("res://addons/kv-vehicle/scene/instantiable/kwheel.gd"), preload("res://addons/kv-vehicle/car-wheel.svg"))
-	add_custom_type('KVVehicleInstantiator', 'KVVehicleInstantiator', preload("res://addons/kv-vehicle/scene/instantiable/kv-vehicleInstantiator.gd"), preload("res://addons/kv-vehicle/city-car.svg"))
-	add_custom_type('KVWheelInstantiator', 'KVWheelInstantiator', preload("res://addons/kv-vehicle/scene/instantiable/kwheelinstancer.gd"), preload("res://addons/kv-vehicle/car-wheel.svg"))
+	add_custom_type('KVVehicle', 'KVVehicle', preload("res://addons/kv-vehicle/scene/instantiable/kv-vehicle.gd"), preload("res://addons/kv-vehicle/city-car.svg"))
+	add_custom_type('KVWheel', 'KVWheel', preload("res://addons/kv-vehicle/scene/instantiable/kv-wheel.gd"), preload("res://addons/kv-vehicle/car-wheel.svg"))
+	add_custom_type('KVVehicleInstantiator', 'KVVehicleInstantiator', preload("res://addons/kv-vehicle/nodes/kv-vehicleInstantiator.gd"), preload("res://addons/kv-vehicle/city-car.svg"))
+	add_custom_type('KVWheelInstantiator', 'KVWheelInstantiator', preload("res://addons/kv-vehicle/nodes/kv-wheelInstantiator.gd"), preload("res://addons/kv-vehicle/car-wheel.svg"))
 
 
 func _exit_tree():
